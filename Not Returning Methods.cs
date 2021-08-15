f55d1bfcdb617a797eca5307ab6d842c76e44fd7
aysegulyilmaz5 <yilmazansr@gmail.com>
2021-08-15 21:11:40 +0300

    Create Not Returning Methods.cs

---
+++ Not Returning Methods.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Methods
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Clear()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox1.Focus();
+        }
+
+        void Colored()
+        {
+            textBox1.BackColor = Color.AntiqueWhite;
+            textBox2.BackColor = Color.Aquamarine;
+            textBox3.BackColor = Color.DeepPink;
+            textBox4.BackColor = Color.Firebrick;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Colored();
+        }
+    }
+}
